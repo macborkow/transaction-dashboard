807a11dfe8fb3153489012191bd23f04a11a3a2b
macborkow <macborkow@gmail.com>
2023-04-07 20:56:46 +0200

    init oauth

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { createApp } from 'vue';
+import { createAuth0 } from '@auth0/auth0-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(store).use(router).use(
+  createAuth0({
+    domain: 'dev-x0dtyv186bbbfyta.us.auth0.com',
+    clientId: 'g9CdikxK4FWCUzqZ2oycsPXCBwpVi9BJ',
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+    },
+  }),
+)
+  .mount('#app');
---
+++ src/views/LoginView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="login">Log in</button>
+    <button @click="logout">Log out</button>
+    <pre>
+      <code>{{ user }}</code>
+    </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: this.$auth0.user,
+    };
+  },
+  methods: {
+    login() {
+      this.$auth0.loginWithRedirect();
+    },
+    logout() {
+      this.$auth0.logout({ logoutParams: { returnTo: window.location.origin } });
+    },
+  },
+};
+</script>
