1804db6e208093bbd89c84f428baff213e7a56b6
macborkow <macborkow@gmail.com>
2023-04-09 00:41:50 +0200

    add spinner after refresh

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,22 +1,35 @@
 <template>
   <div>
-    <button v-if="!isAuthenticated" @click="login">Log in</button>
-    <button v-if="isAuthenticated" @click="logout">Log out</button>
-    <pre>
-      <code>{{ user }}</code>
-    </pre>
+    <div v-if='!isAuthenticated'>
+      <button v-if="!isLoading" @click="login">Log in</button>
+      <Spinner v-else />
+    </div>
+    <div v-if='isAuthenticated'>
+      <button @click="logout">Log out</button>
+      <pre>
+        <code>{{ user }}</code>
+      </pre>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Spinner from '@/components/Spinner.vue';
 
 export default defineComponent({
+  components: {
+    Spinner,
+  },
   data() {
     return {
       user: this.$auth0.user,
       isAuthenticated: this.$auth0.isAuthenticated,
+      isLoading: this.$auth0.isLoading,
     };
+  },
+  mounted() {
+    console.log(this.$auth0);
   },
   methods: {
     login() {
