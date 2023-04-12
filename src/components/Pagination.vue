fce8cc459d6dff097582310313ee6588ebfbd416
macborkow <macborkow@gmail.com>
2023-04-12 14:38:29 +0200

    refine pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,16 +1,28 @@
 <template>
-  <button @click="changePage(0, false)">
+  <nav v-if="lastPage > 0">
+  <button v-if="currentPage > 1" @click="changePage(0, false)">
   ◄◄
   </button>
-  <button @click=changePage(-1)>
+  <button v-if="currentPage > 0" @click=changePage(-1)>
   ◄
   </button>
-  <button @click=changePage(1)>
+  <button v-for="item in quickNavigationPages" v-bind:key=item.id @click="changePage(item, false)">
+    {{ item + 1 }}
+  </button>
+  <button v-if="currentPage < lastPage" @click=changePage(1)>
   ►
   </button>
-  <button @click="changePage(lastPage, false)">
+  <button v-if="currentPage < lastPage - 1" @click="changePage(lastPage, false)">
   ►►
   </button>
+  </nav>
+  <label for="amountRows">Rows to show
+    <select @change=updateRows name="amountRows">
+      <option value=5>5</option>
+      <option value=10>10</option>
+    </select>
+  </label>
+  <p> {{ currentPage + 1 }}/{{ lastPage + 1 }} </p>
 </template>
 
 <script lang="ts">
@@ -34,11 +46,19 @@ export default defineComponent({
     visibleData() {
       const start = this.numberOfRows * this.currentPage;
       const end = start + this.numberOfRows;
-      console.log(this.data.slice(start, end));
       return this.data.slice(start, end);
     },
     lastPage() {
       return Math.floor((this.data.length - 1) / this.numberOfRows);
+    },
+    quickNavigationPages() {
+      const from = Math.max(this.currentPage - 2, 0);
+      const to = Math.min(this.currentPage + 2, this.lastPage);
+      const output = [];
+      for (let i = from; i <= to; i += 1) {
+        output.push(i);
+      }
+      return output;
     },
   },
   watch: {
@@ -50,6 +70,14 @@ export default defineComponent({
     },
   },
   methods: {
+    updateRows(event) {
+      const newValue = parseInt(event.target.value, 10);
+      const preservedRatio = this.numberOfRows / newValue;
+      const newCurrentPage = this.currentPage * preservedRatio;
+      this.currentPage = Math.floor(newCurrentPage);
+      this.numberOfRows = newValue;
+      this.truncate();
+    },
     changePage(howMuch, relative = true) {
       if (relative) {
         this.currentPage += howMuch;
