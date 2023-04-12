1cf79f61b55ae7895cb69b9219c7435f4960bb86
macborkow <macborkow@gmail.com>
2023-04-12 10:16:17 +0200

    cleaup customersearch

---
+++ src/components/CustomerSearch.vue
@@ -0,0 +1,42 @@
+<template>
+  <label for="searchCustomer">Search Customer
+    <input @input=filterData v-model=search type="text" />
+  </label>
+  <p> {{ error }} </p>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { Customer } from '@/common/types';
+
+export default defineComponent({
+  name: 'CustomerSearch',
+  emits: ['filtered'],
+  props: {
+    data: {
+      type: Array as PropType<Customer[]>,
+    },
+  },
+  data() {
+    return {
+      search: null,
+    };
+  },
+  computed: {
+    filteredData() {
+      return this.data;
+    },
+    error() {
+      return '';
+    },
+  },
+  methods: {
+    filterData() {
+      this.$emit('filtered', this.filteredData);
+    },
+  },
+});
+</script>
+
+<style scoped>
+</style>
